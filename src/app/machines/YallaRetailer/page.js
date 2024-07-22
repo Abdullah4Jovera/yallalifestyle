a66f88073d637da364feffe2deba53e8c6dbe85a
Abdullah4Jovera <165145974+Abdullah4Jovera@users.noreply.github.com>
2024-07-22 18:02:22 +0400

    Update page.js

--- src/app/machines/YallaRetailer/page.js
+++ src/app/machines/YallaRetailer/page.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import YallaMachineNavbar from '../YallaMachineNavbar'
 
-const page = () => {
+const Page = () => {
     return (
         <div>
             <YallaMachineNavbar />
@@ -9,4 +9,4 @@ const page = () => {
     )
 }
 
-export default page
+export default Page
