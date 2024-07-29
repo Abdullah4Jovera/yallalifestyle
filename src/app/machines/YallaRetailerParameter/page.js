a4aa9d2df24fc7eb61a968f1fa71cf453ebbdd10
Abdullah4Jovera <165145974+Abdullah4Jovera@users.noreply.github.com>
2024-07-29 18:46:30 +0400

    c
    c

--- src/app/machines/YallaRetailerParameter/page.js
+++ src/app/machines/YallaRetailerParameter/page.js
@@ -2,16 +2,15 @@
 import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import Image from 'next/image';
-import YallaMachineNavbar from '../YallaMachineNavbar';
-import yallalifestyleparamter from '../../Assets/MachinePageAssets/yallalifestyleparamter.png';
 import '../YallaHealther/Yallahealther.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import YallaFooter from '@/app/YallaFooter';
 import Link from 'next/link';
-import backlogo from '../../Assets/MachinePageAssets/backlogo.png';
+import yallalifestyleparamter from '../../Assets/MachinePageAssets/yallalifestyleparamter.png';
 import { MdOutlineKeyboardBackspace } from "react-icons/md";
-import HomePageNav from '../../HomePageNav'
+import HomePageNav from '../../HomePageNav';
+
 const Page = () => {
     useEffect(() => {
         AOS.init({
@@ -23,10 +22,21 @@ const Page = () => {
         <>
            <HomePageNav />
             <div className='machineConatiner_parameter'>
-                <Container >
-                    <Link href='/machines'><MdOutlineKeyboardBackspace data-aos="fade-up" className='mt-4' style={{ color: 'black', fontSize:'50px' }} /></Link>
-                    <h1 className='gohealthier_parameter' data-aos="fade-up" >LIFESTYLE</h1>
-                    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} data-aos="fade-up" >
+                <Container>
+                    <Link href='/machines'>
+                        <MdOutlineKeyboardBackspace 
+                            data-aos="fade-up" 
+                            className='mt-4' 
+                            style={{ color: 'black', fontSize:'50px' }} 
+                        />
+                    </Link>
+                    <h1 className='gohealthier_parameter' data-aos="fade-up">
+                        LIFESTYLE
+                    </h1>
+                    <div 
+                        style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }} 
+                        data-aos="fade-up"
+                    >
                         <Image
                             src={yallalifestyleparamter}
                             alt='yallalifestyleparamter'
@@ -35,9 +45,9 @@ const Page = () => {
                         />
                     </div>
 
-                    <div className='h1_conatiner_healthier_tag' >
-                        <h1 className='yallahealtier_h1_tag' data-aos="fade-up"  >
-                            <span className='go_healthier_H_WORD' >Y</span>alla's"LIFESTYLE" Vending machines: offering curated, wholesome snacks for health-conscious consumers on the go. We prioritize taste and nutrition, ensuring each product meets our high standards. Empower your wellness journey with convenient, quality choices that support a healthier lifestyle.
+                    <div className='h1_conatiner_healthier_tag'>
+                        <h1 className='yallahealtier_h1_tag' data-aos="fade-up">
+                            <span className='go_healthier_H_WORD'>Y</span>alla&apos;s &quot;LIFESTYLE&quot; Vending machines: offering curated, wholesome snacks for health-conscious consumers on the go. We prioritize taste and nutrition, ensuring each product meets our high standards. Empower your wellness journey with convenient, quality choices that support a healthier lifestyle.
                         </h1>
                     </div>
 
